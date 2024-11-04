d4579c889d349157e9083c2f07c26a3eaf4b57cd
lorenavigano <viganolorena08@gmail.com>
2024-11-04 16:05:04 -0300

    funcionarios

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $usuario = $_POST ['usuario'];
+    $senha = $_POST ['senha'];
+
+    include "conexao.php";
+
+    $sql = "SELECT * FROM funcionarios
+            WHERE usuario = '$usuario'
+            AND senha = '$senha'";
+
+    $resultado = mysqli_query ($con, $sql);
+
+    if (mysqli_num_rows($resultado) > 0) {
+    echo "Liberado!";
+    header ('location: inicio.html');
+} else {
+    echo "Usuário ou senha inválidos!";
+    echo "<a href= 'index.html'>Voltar</a>";
+    }
+}
